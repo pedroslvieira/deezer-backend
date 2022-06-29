642a7bce70b0494220fda378d22da2140f0e0d54
Pedro Soares Lima Vieira <pedroslvieira@gmail.com>
2022-06-29 19:59:08 +0200

    response headers configured

--- app/controllers/api/v1/tracks_controller.rb
+++ app/controllers/api/v1/tracks_controller.rb
@@ -5,8 +5,9 @@ class Api::V1::TracksController < Api::V1::BaseController
   def index
     uri = URI('https://api.deezer.com/chart/0/tracks?limit=100')
     res = Net::HTTP.get_response(uri)
-    # res.header('Access-Control-Allow-Origin', '*')
+    response.body = res.body
+    response.set_header('Access-Control-Allow-Origin', '*')
     # raise
-    render json: res.body
+    render json: response.body
   end
 end
