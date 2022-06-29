2fa51a65d1367f44b3e6cf3790143766d7a0be7e
Pedro Soares Lima Vieira <pedroslvieira@gmail.com>
2022-06-29 21:33:40 +0200

    api request limited to 10

--- app/controllers/api/v1/tracks_controller.rb
+++ app/controllers/api/v1/tracks_controller.rb
@@ -3,7 +3,7 @@ require 'net/http'
 
 class Api::V1::TracksController < Api::V1::BaseController
   def index
-    uri = URI('https://api.deezer.com/chart/0/tracks?limit=100')
+    uri = URI('https://api.deezer.com/chart/0/tracks')
     res = Net::HTTP.get_response(uri)
     response.body = res.body
     response.set_header('Access-Control-Allow-Origin', '*')
